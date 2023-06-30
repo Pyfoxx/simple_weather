28d86d123cae13d110dc6e3718c7f6e59a567dc5
Pyfox <59779920+Pyfoxx@users.noreply.github.com>
2023-06-30 10:20:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ import requests, json
 def request_data():
 	city = 'bordeaux'
 	unit = 'metric
-	key = '4cba0b113794f3bad84b2b022be28215'
-	# request = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}'
 	request = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units={unit}"
-	print(request)
 	r = requests.get(request)
 	return r.json()
 
